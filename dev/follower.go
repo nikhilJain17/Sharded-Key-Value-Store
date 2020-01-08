24226d0496049a0f3f4931d97a9db35aa9a1ad85
nikhil.jain0987@gmail.com <nikhil.jain0987@gmail.com>
2020-01-07 19:56:15 -0500

    stood up basic kv store over http! time to clean up code a bit, then start the distributed stuff

--- dev/follower.go
+++ dev/follower.go
@@ -7,7 +7,18 @@ import (
 	"net/http"
 	// "regexp"
 	"fmt"
+	// "encoding/gob"
+	// "bytes"
+	"github.com/boltdb/bolt"
+	"time"
 )
+
+type KVPair struct {
+	Key string
+	Value string
+}
+
+var db *bolt.DB
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello world")
@@ -32,6 +43,18 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	
 	/* now get from persistent storage */
 
+	// access db
+	// db, err := bolt.Open("kvstore.db", 0600, nil)
+
+	// read
+	db.View(func(transaction *bolt.Tx) error {
+		bucket := transaction.Bucket([]byte("kvbucket"))
+		val := bucket.Get( []byte(key) ) // @todo copy the bytes over to a new slice...because val is dead when xact is dead
+		fmt.Fprintf(w, string(val))
+		return nil
+	})
+
+
 }
 
 func putHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,9 +78,55 @@ func putHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println("[put] key:", key, ", value:", val)
 
+	// @todo some validation here also
+
+	/* store in persistent storage probably */
+
+	// @todo encoding to bytes? (look into gob)
+
+	// var buf bytes.Buffer
+	// encoder := gob.NewEncoder(&buf)
+	// kvpair := KVPair{
+	// 	Key: key,
+	// 	Value: val,
+	// }
+	// encoder.Encode(kvpair)
+	// encodedBytes := buf.Bytes()
+
+	// keyBytes := 
+
+	// fmt.Println("sankruth:", encodedBytes)
+
+
+	// store in a db? sqlite, boltdb...
+	// the point of this project is to do the distributed part, not really the kv store part.
+	// i don't want to implement a database, i want to implement the distributed algos
+	// so we'll just roll with boltdb instead of storing to a sorted file or something lol
+
+	db.Update(func(transaction *bolt.Tx) error {
+		bucket, err := transaction.CreateBucketIfNotExists([]byte("kvbucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		} 
+		err = bucket.Put([]byte(key), []byte(val))
+		return err
+	})
+
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	// @todo
+	
 }
 
 func main() {
+
+	/* set up boltdb */
+	var err error
+	db, err = bolt.Open("kvstore.db", 0600, &bolt.Options{Timeout: 2 * time.Second})
+	if err != nil {log.Fatal("error opening boltdb: ", err)}
+	defer db.Close() // defer is pretty handy
+
 	// hello world handler
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
@@ -66,5 +135,6 @@ func main() {
 	http.HandleFunc("/test", testHandler)
 	http.HandleFunc("/get", getHandler)
 	http.HandleFunc("/put", putHandler)
+	http.HandleFunc("/delete", deleteHandler)
 	http.ListenAndServe(":8080", nil)
 }
